2825e28c707ff617d29e66b11b81451192ccc51a
Jose-amlo <82465006+Jose-amlo@users.noreply.github.com>
2023-09-29 04:50:13 -0300

    Actualización de funcionamiento P1
    se agregaron los visit de la actualización del estado/modo (encendido y apagado) y visitPos

--- ControlP1/MyVisitor.py
+++ ControlP1/MyVisitor.py
@@ -5,6 +5,11 @@ from turtle import *
 class MyVisitor(PrimeraParteVisitor):
     def __init__(self):
         self.memory = {}
+
+    def visitMod(self, ctx):
+        value = self.visit(ctx.modo())
+        print(value)
+        return 0
     
     def visitDib(self, ctx):
         value = self.visit(ctx.puntero())
@@ -13,16 +18,19 @@ class MyVisitor(PrimeraParteVisitor):
     
     def visitINT(self, ctx):
         return ctx.INT().getText()
+
+    def visitAsignMod(self, ctx):
+        if ctx.op.type == PrimeraParteParser.ENC:
+            pendown()
+            return 0
+        else:
+            penup()
+            return 0
     
     def visitPos(self, ctx):
         coordX = int(self.visit(ctx.puntero(0)))
         coordY = int(self.visit(ctx.puntero(1)))
         hideturtle()
-        if ctx.op.type == PrimeraParteParser.ENC:
-            pendown()
-            setpos(coordX,coordY)
-            return 0
-        penup()
         setpos(coordX,coordY)
         return 0
     
