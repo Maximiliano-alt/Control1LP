7483039019a702f260c0e1e044ebd4ebb7e8ccaf
Maximiliano Espindola <74783377+Maximiliano-alt@users.noreply.github.com>
2023-09-29 20:14:15 -0300

    change in grammatic

--- ControlP1/PrimeraParteParser.py
+++ ControlP1/PrimeraParteParser.py
@@ -10,15 +10,17 @@ else:
 
 def serializedATN():
     return [
-        4,1,8,28,2,0,7,0,2,1,7,1,2,2,7,2,1,0,4,0,8,8,0,11,0,12,0,9,1,1,1,
-        1,1,1,1,1,3,1,16,8,1,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,3,2,26,8,2,
-        1,2,0,0,3,0,2,4,0,1,1,0,4,5,27,0,7,1,0,0,0,2,15,1,0,0,0,4,25,1,0,
-        0,0,6,8,3,2,1,0,7,6,1,0,0,0,8,9,1,0,0,0,9,7,1,0,0,0,9,10,1,0,0,0,
-        10,1,1,0,0,0,11,12,3,4,2,0,12,13,5,7,0,0,13,16,1,0,0,0,14,16,5,7,
-        0,0,15,11,1,0,0,0,15,14,1,0,0,0,16,3,1,0,0,0,17,18,5,1,0,0,18,19,
-        3,4,2,0,19,20,5,2,0,0,20,21,3,4,2,0,21,22,5,3,0,0,22,23,7,0,0,0,
-        23,26,1,0,0,0,24,26,5,6,0,0,25,17,1,0,0,0,25,24,1,0,0,0,26,5,1,0,
-        0,0,3,9,15,25
+        4,1,9,35,2,0,7,0,2,1,7,1,2,2,7,2,2,3,7,3,1,0,4,0,10,8,0,11,0,12,
+        0,11,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,21,8,1,1,2,1,2,1,3,1,3,1,3,
+        1,3,1,3,1,3,1,3,1,3,3,3,33,8,3,1,3,0,0,4,0,2,4,6,0,1,1,0,4,5,34,
+        0,9,1,0,0,0,2,20,1,0,0,0,4,22,1,0,0,0,6,32,1,0,0,0,8,10,3,2,1,0,
+        9,8,1,0,0,0,10,11,1,0,0,0,11,9,1,0,0,0,11,12,1,0,0,0,12,1,1,0,0,
+        0,13,14,3,4,2,0,14,15,5,8,0,0,15,21,1,0,0,0,16,17,3,6,3,0,17,18,
+        5,8,0,0,18,21,1,0,0,0,19,21,5,8,0,0,20,13,1,0,0,0,20,16,1,0,0,0,
+        20,19,1,0,0,0,21,3,1,0,0,0,22,23,7,0,0,0,23,5,1,0,0,0,24,25,5,6,
+        0,0,25,26,5,1,0,0,26,27,3,6,3,0,27,28,5,2,0,0,28,29,3,6,3,0,29,30,
+        5,3,0,0,30,33,1,0,0,0,31,33,5,7,0,0,32,24,1,0,0,0,32,31,1,0,0,0,
+        33,7,1,0,0,0,3,11,20,32
     ]
 
 class PrimeraParteParser ( Parser ):
@@ -31,16 +33,18 @@ class PrimeraParteParser ( Parser ):
 
     sharedContextCache = PredictionContextCache()
 
-    literalNames = [ "<INVALID>", "'('", "','", "')'", "'encendido'", "'apagado'" ]
+    literalNames = [ "<INVALID>", "'('", "','", "')'", "'encendido'", "'apagado'", 
+                     "'mover'" ]
 
     symbolicNames = [ "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                      "ENC", "APAG", "INT", "NEWLINE", "WS" ]
+                      "ENC", "APAG", "MOV", "INT", "NEWLINE", "WS" ]
 
     RULE_prog = 0
     RULE_dibujo = 1
-    RULE_puntero = 2
+    RULE_modo = 2
+    RULE_puntero = 3
 
-    ruleNames =  [ "prog", "dibujo", "puntero" ]
+    ruleNames =  [ "prog", "dibujo", "modo", "puntero" ]
 
     EOF = Token.EOF
     T__0=1
@@ -48,9 +52,10 @@ class PrimeraParteParser ( Parser ):
     T__2=3
     ENC=4
     APAG=5
-    INT=6
-    NEWLINE=7
-    WS=8
+    MOV=6
+    INT=7
+    NEWLINE=8
+    WS=9
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
@@ -94,16 +99,16 @@ class PrimeraParteParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 7 
+            self.state = 9 
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             while True:
-                self.state = 6
+                self.state = 8
                 self.dibujo()
-                self.state = 9 
+                self.state = 11 
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
-                if not ((((_la) & ~0x3f) == 0 and ((1 << _la) & 194) != 0)):
+                if not ((((_la) & ~0x3f) == 0 and ((1 << _la) & 496) != 0)):
                     break
 
         except RecognitionException as re:
@@ -130,6 +135,25 @@ class PrimeraParteParser ( Parser ):
         def copyFrom(self, ctx:ParserRuleContext):
             super().copyFrom(ctx)
 
+
+
+    class ModContext(DibujoContext):
+
+        def __init__(self, parser, ctx:ParserRuleContext): # actually a PrimeraParteParser.DibujoContext
+            super().__init__(parser)
+            self.copyFrom(ctx)
+
+        def modo(self):
+            return self.getTypedRuleContext(PrimeraParteParser.ModoContext,0)
+
+        def NEWLINE(self):
+            return self.getToken(PrimeraParteParser.NEWLINE, 0)
+
+        def accept(self, visitor:ParseTreeVisitor):
+            if hasattr( visitor, "visitMod" ):
+                return visitor.visitMod(self)
+            else:
+                return visitor.visitChildren(self)
 
 
     class BlankContext(DibujoContext):
@@ -173,26 +197,96 @@ class PrimeraParteParser ( Parser ):
         localctx = PrimeraParteParser.DibujoContext(self, self._ctx, self.state)
         self.enterRule(localctx, 2, self.RULE_dibujo)
         try:
-            self.state = 15
+            self.state = 20
             self._errHandler.sync(self)
             token = self._input.LA(1)
-            if token in [1, 6]:
-                localctx = PrimeraParteParser.DibContext(self, localctx)
+            if token in [4, 5]:
+                localctx = PrimeraParteParser.ModContext(self, localctx)
                 self.enterOuterAlt(localctx, 1)
-                self.state = 11
-                self.puntero()
-                self.state = 12
+                self.state = 13
+                self.modo()
+                self.state = 14
                 self.match(PrimeraParteParser.NEWLINE)
                 pass
-            elif token in [7]:
-                localctx = PrimeraParteParser.BlankContext(self, localctx)
+            elif token in [6, 7]:
+                localctx = PrimeraParteParser.DibContext(self, localctx)
                 self.enterOuterAlt(localctx, 2)
-                self.state = 14
+                self.state = 16
+                self.puntero()
+                self.state = 17
+                self.match(PrimeraParteParser.NEWLINE)
+                pass
+            elif token in [8]:
+                localctx = PrimeraParteParser.BlankContext(self, localctx)
+                self.enterOuterAlt(localctx, 3)
+                self.state = 19
                 self.match(PrimeraParteParser.NEWLINE)
                 pass
             else:
                 raise NoViableAltException(self)
 
+        except RecognitionException as re:
+            localctx.exception = re
+            self._errHandler.reportError(self, re)
+            self._errHandler.recover(self, re)
+        finally:
+            self.exitRule()
+        return localctx
+
+
+    class ModoContext(ParserRuleContext):
+        __slots__ = 'parser'
+
+        def __init__(self, parser, parent:ParserRuleContext=None, invokingState:int=-1):
+            super().__init__(parent, invokingState)
+            self.parser = parser
+
+
+        def getRuleIndex(self):
+            return PrimeraParteParser.RULE_modo
+
+     
+        def copyFrom(self, ctx:ParserRuleContext):
+            super().copyFrom(ctx)
+
+
+
+    class AsignModContext(ModoContext):
+
+        def __init__(self, parser, ctx:ParserRuleContext): # actually a PrimeraParteParser.ModoContext
+            super().__init__(parser)
+            self.op = None # Token
+            self.copyFrom(ctx)
+
+        def ENC(self):
+            return self.getToken(PrimeraParteParser.ENC, 0)
+        def APAG(self):
+            return self.getToken(PrimeraParteParser.APAG, 0)
+
+        def accept(self, visitor:ParseTreeVisitor):
+            if hasattr( visitor, "visitAsignMod" ):
+                return visitor.visitAsignMod(self)
+            else:
+                return visitor.visitChildren(self)
+
+
+
+    def modo(self):
+
+        localctx = PrimeraParteParser.ModoContext(self, self._ctx, self.state)
+        self.enterRule(localctx, 4, self.RULE_modo)
+        self._la = 0 # Token type
+        try:
+            localctx = PrimeraParteParser.AsignModContext(self, localctx)
+            self.enterOuterAlt(localctx, 1)
+            self.state = 22
+            localctx.op = self._input.LT(1)
+            _la = self._input.LA(1)
+            if not(_la==4 or _la==5):
+                localctx.op = self._errHandler.recoverInline(self)
+            else:
+                self._errHandler.reportMatch(self)
+                self.consume()
         except RecognitionException as re:
             localctx.exception = re
             self._errHandler.reportError(self, re)
@@ -223,19 +317,16 @@ class PrimeraParteParser ( Parser ):
 
         def __init__(self, parser, ctx:ParserRuleContext): # actually a PrimeraParteParser.PunteroContext
             super().__init__(parser)
-            self.op = None # Token
             self.copyFrom(ctx)
 
+        def MOV(self):
+            return self.getToken(PrimeraParteParser.MOV, 0)
         def puntero(self, i:int=None):
             if i is None:
                 return self.getTypedRuleContexts(PrimeraParteParser.PunteroContext)
             else:
                 return self.getTypedRuleContext(PrimeraParteParser.PunteroContext,i)
 
-        def ENC(self):
-            return self.getToken(PrimeraParteParser.ENC, 0)
-        def APAG(self):
-            return self.getToken(PrimeraParteParser.APAG, 0)
 
         def accept(self, visitor:ParseTreeVisitor):
             if hasattr( visitor, "visitPos" ):
@@ -264,38 +355,31 @@ class PrimeraParteParser ( Parser ):
     def puntero(self):
 
         localctx = PrimeraParteParser.PunteroContext(self, self._ctx, self.state)
-        self.enterRule(localctx, 4, self.RULE_puntero)
-        self._la = 0 # Token type
+        self.enterRule(localctx, 6, self.RULE_puntero)
         try:
-            self.state = 25
+            self.state = 32
             self._errHandler.sync(self)
             token = self._input.LA(1)
-            if token in [1]:
+            if token in [6]:
                 localctx = PrimeraParteParser.PosContext(self, localctx)
                 self.enterOuterAlt(localctx, 1)
-                self.state = 17
+                self.state = 24
+                self.match(PrimeraParteParser.MOV)
+                self.state = 25
                 self.match(PrimeraParteParser.T__0)
-                self.state = 18
+                self.state = 26
                 self.puntero()
-                self.state = 19
+                self.state = 27
                 self.match(PrimeraParteParser.T__1)
-                self.state = 20
+                self.state = 28
                 self.puntero()
-                self.state = 21
+                self.state = 29
                 self.match(PrimeraParteParser.T__2)
-                self.state = 22
-                localctx.op = self._input.LT(1)
-                _la = self._input.LA(1)
-                if not(_la==4 or _la==5):
-                    localctx.op = self._errHandler.recoverInline(self)
-                else:
-                    self._errHandler.reportMatch(self)
-                    self.consume()
                 pass
-            elif token in [6]:
+            elif token in [7]:
                 localctx = PrimeraParteParser.INTContext(self, localctx)
                 self.enterOuterAlt(localctx, 2)
-                self.state = 24
+                self.state = 31
                 self.match(PrimeraParteParser.INT)
                 pass
             else:
